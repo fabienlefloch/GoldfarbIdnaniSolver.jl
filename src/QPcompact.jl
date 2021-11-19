fe6cbdee6e8bba305aa88faf168b3443cb8174ea
Fabien Le Floc'h <fabien@2ipi.com>
2021-11-19 12:11:54 +0100

    cleanup license
    Change-Id: Iaedf5801433d3abf09e8e3a23e62cd691ea88e45

--- src/QPcompact.jl
+++ src/QPcompact.jl
@@ -80,21 +80,6 @@ end
 #  Original Fortran Code
 #  Copyright (C) 1995-2010 Berwin A. Turlach <Berwin.Turlach@gmail.com>
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-#  USA.
-#
 #  this routine uses the Goldfarb/Idnani algorithm to solve the
 #  following minimization problem:
 #
