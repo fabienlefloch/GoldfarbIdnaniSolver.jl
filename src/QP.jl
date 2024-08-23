bcbf9ae40c4bb3336cef50316596256c9f0f6d5f
Fabien Le Floc'h <fabien@2ipi.com>
2024-08-23 13:08:20 +0200

    prettier doc

--- src/QP.jl
+++ src/QP.jl
@@ -2,6 +2,8 @@ using LinearAlgebra
 export solveQP
 
 """
+    solveQP(dmat::AbstractMatrix{T}, dvec::AbstractArray{T}, Amat::AbstractMatrix{T}, bvec::AbstractArray{T}; meq::Int = 0, factorized::Bool = false)::Tuple{AbstractArray{T},AbstractArray{T},T,AbstractArray{Int},Int,AbstractArray{Int}}
+
 This routine implements the dual method of Goldfarb and Idnani (1982, 1983) for solving quadratic programming problems of the form
 
          minimize  -d^T x + 1/2 *  x^T D x
@@ -11,8 +13,8 @@ This routine implements the dual method of Goldfarb and Idnani (1982, 1983) for 
    the matrix D is assumed to be positive definite.  Especially,
    w.l.o.g. D is assumed to be symmetric.
    
-   Input parameter:
-   * dmat   nxn matrix, the matrix D from above (dp)
+  # Input parameter:
+   * `dmat`   nxn matrix, the matrix D from above (typically Float64)
           The user has two possibilities:
           a) Give D (factorized = false), in this case we use routines from LINPACK
              to decompose D.
@@ -20,26 +22,26 @@ This routine implements the dual method of Goldfarb and Idnani (1982, 1983) for 
              So if it is cheaper to calculate R^-1 in another way (D may
              be a band matrix) then with the general routine, the user
              may pass R^{-1}.  Indicated by factorized = true.
-   * dvec   nx1 vector, the vector d from above (dp)
+   * `dvec`   nx1 vector, the vector d from above (typically Float64)
           contains on exit the solution to the initial, i.e.,
           unconstrained problem
-   * Amat   nxq matrix, the matrix A from above (dp) [ A=(A1 A2)^T ]
+   * `Amat`   nxq matrix, the matrix A from above (typically Float64) [ A=(A1 A2)^T ]
            ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE
               CHANGED SIGNES ON EXIT.
-   * bvec   qx1 vector, the vector of constants b in the constraints (dp)
+   * `bvec`   qx1 vector, the vector of constants b in the constraints (typically Float64)
           [ b = (b1^T b2^T)^T ]
           ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE
               CHANGED SIGNES ON EXIT.
-   * meq    integer, the number of equality constraints, 0 <= meq <= q.
+   * `meq::Int=0`  the number of equality constraints, 0 <= meq <= q.
  
-   Output parameter is a Tuple composed of:
-   * sol   nx1 the final solution (x in the notation above)
-   * lagr  qx1 the final Lagrange multipliers
-   * crval scalar, the value of the criterion at the minimum      
-   * iact  qx1 vector, the constraints which are active in the final
+ # Output parameter is a Tuple composed of:
+   1. sol   nx1 the final solution (x in the notation above)
+   2. lagr  qx1 the final Lagrange multipliers
+   3. crval scalar, the value of the criterion at the minimum      
+   4. iact  qx1 vector, the constraints which are active in the final
          fit (int)
-   * nact  scalar, the number of constraints active in the final fit (int)
-   * iter  2x1 vector, first component gives the number of "main" 
+   5. nact  scalar, the number of constraints active in the final fit (int)
+   6. iter  2x1 vector, first component gives the number of "main" 
          iterations, the second one says how many constraints were
          deleted after they became active
 """
--- src/QPcompact.jl
+++ src/QPcompact.jl
@@ -2,6 +2,8 @@ using LinearAlgebra, SparseArrays
 export solveQPcompact, convertSparse
 
 """
+    solveQPcompact(dmat::AbstractMatrix{T}, dvec::AbstractArray{T}, Amat::AbstractMatrix{T}, Aind::AbstractMatrix{Int}, bvec::AbstractArray{T}; meq::Int = 0, factorized::Bool = false)::Tuple{AbstractArray{T},AbstractArray{T},T,AbstractArray{Int},Int,AbstractArray{Int}}
+
   This routine uses the Goldfarb/Idnani algorithm to solve the
   following minimization problem:
 
@@ -12,8 +14,8 @@ export solveQPcompact, convertSparse
   the matrix D is assumed to be positive definite.  Especially,
   w.l.o.g. D is assumed to be symmetric.
 
-  Input parameter:
-  * dmat   nxn matrix, the matrix D from above (dp)
+  # Input parameter
+  * `dmat`   nxn matrix, the matrix D from above (dp)
          The user has two possibilities:
          a) Give D (factorized=false), in this case we use routines from LINPACK
             to decompose D.
@@ -21,13 +23,13 @@ export solveQPcompact, convertSparse
             So if it is cheaper to calculate R^-1 in another way (D may
             be a band matrix) then with the general routine, the user
             may pass R^{-1}.  Indicated by factorized = true.
-  * dvec   nx1 vector, the vector d from above (dp)
+  * `dvec`   nx1 vector, the vector d from above (typically Float64)
          contains on exit the solution to the initial, i.e.,
          unconstrained problem
-  * Amat   lxq matrix (dp).
+  * `Amat`   lxq matrix (typically of Float64).
           ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE
              CHANGED SIGNES ON EXIT. 
-  * Aind  (l+1)xq matrix (int)
+  * `Aind`  (l+1)xq matrix (Int)
          these two matrices store the matrix A in compact form. the format
          is: [ A=(A1 A2)^T ]
            Aind(1,i) is the number of non-zero elements in column i of A.
@@ -35,23 +37,23 @@ export solveQPcompact, convertSparse
                       element in column i of A is A(i,j).
            Aind(k,i) for k>=1, is equal to the k-th non-zero element
                       in column i of A.
-  * bvec   qx1 vector, the vector of constants b in the constraints (dp)
+  * `bvec`   qx1 vector, the vector of constants b in the constraints (typically Float64)
          [ b = (b1^T b2^T)^T ]
-         *** ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE
-             CHANGED SIGNES ON EXIT ***
-  * meq    integer, the number of equality constraints, 0 <= meq <= q.
+          ENTRIES CORRESPONDING TO EQUALITY CONSTRAINTS MAY HAVE CHANGED SIGNES ON EXIT
+  * `meq::Int=0`   the number of equality constraints, 0 <= meq <= q.
 
-  Output parameters as a Tuple:
-  * sol   nx1 the final solution (x in the notation above)
-  * lagr  qx1 the final Lagrange multipliers
-  * crval scalar, the value of the criterion at the minimum
-  * iact  qx1 vector, the constraints which are active in the final
+  # Output parameters as a Tuple
+  1. sol   nx1 the final solution (x in the notation above)
+  2. lagr  qx1 the final Lagrange multipliers
+  3. crval scalar, the value of the criterion at the minimum
+  4. iact  qx1 vector, the constraints which are active in the final
         fit (int)
-  * nact  scalar, the number of constraints active in the final fit (int)
-  * iter  2x1 vector, first component gives the number of "main"
+  5. nact  scalar, the number of constraints active in the final fit (int)
+  6. iter  2x1 vector, first component gives the number of "main"
         iterations, the second one says how many constraints were
         deleted after they became active
 
+    # Tip
 Amat and Aind may be created from a Julia sparse matrix through the convertSparse(A) function where A is a standard Julia sparse matrix.
 """
 function solveQPcompact(dmat::AbstractMatrix{T}, dvec::AbstractArray{T},
@@ -98,7 +100,7 @@ Create Amat and Aind for use in solveQPcompact from a Julia sparse matrix.
 
     Input:
         * dmat: a Julia sparse matrix
-        
+
     Ouput as a tuple:
         * Amat
         * Aind
